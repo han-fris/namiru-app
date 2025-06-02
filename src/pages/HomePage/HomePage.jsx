df17d662d79b75de3d3ccc3ba93e8186667669cf
Han Frisonsova <hanafrisonsova@gmail.com>
2025-06-02 11:14:41 +0200

    zakladni upravy a stylovani

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import './HomePage.css';
+
+export const HomePage = () => {
+  return (
+    <div className="container">
+      <header>
+        <h1>Namiru</h1>
+      </header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+};
