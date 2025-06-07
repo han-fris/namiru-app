3ff9d54afdde7075269487e5df35d79c07bc7855
Han Frisonsova <hanafrisonsova@gmail.com>
2025-06-07 09:59:11 +0200

    oprava importu

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
 import { HomePage } from './pages/HomePage/HomePage';
 import './global.css';
 
