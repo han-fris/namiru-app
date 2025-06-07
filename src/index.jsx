a6bb4ca7b839f4bb61093f395c1d6a6ebf5352cf
Han Frisonsova <hanafrisonsova@gmail.com>
2025-06-07 09:44:17 +0200

    vytvoreni routeru a cest

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,39 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { HomePage } from './pages/HomePage/HomePage';
 import './global.css';
 
 createRoot(document.querySelector('#app')).render(<HomePage />);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/blog',
+    element: <div>blog</div>,
+  },
+  {
+    path: '/blog/:id',
+    element: <div>blogDetail</div>,
+  },
+  {
+    path: '/tvurci',
+    element: <div>tvurci</div>,
+  },
+  {
+    path: '/tvurci/:id',
+    element: <div>tvurciId</div>,
+  },
+  {
+    path: '*',
+    element: <div>404</div>,
+  },
+]);
+
+createRoot(document.querySelector('#app')).render(
+  <RouterProvider router={router} />,
+);
