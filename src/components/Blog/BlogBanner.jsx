aa82f06c2bb2cf66cc32c32ece054fea4bd19289
Katarina Packova <katarina.packova@gmail.com>
2025-06-07 17:51:40 +0200

    BlogBanner styling updated

--- src/components/Blog/BlogBanner.jsx
+++ src/components/Blog/BlogBanner.jsx
@@ -1,12 +1,8 @@
 import { Button } from '../shared/Button';
-import img1 from './../../../public/img/eco1.jpg';
-import img2 from './../../../public/img/man2.jpg';
-import img3 from './../../../public/img/pus2.jpg';
-import img4 from './../../../public/img/trans2.jpg';
-import img5 from './../../../public/img/dis1.jpg';
+import img1 from './../../../public/img/trans2.jpg';
 import './BlogBanner.css';
 
-const images = [img1, img2, img3, img4, img5];
+/* const images = [img1, img2, img3, img4, img5]; */
 
 export const BlogBanner = ({
   buttonText,
@@ -16,18 +12,19 @@ export const BlogBanner = ({
   title,
   paragraph,
   imgPosition,
+  index,
 }) => {
-  const randomIndex = Math.floor(Math.random() * images.length);
-  const randomImg = images[randomIndex];
+  /* const randomIndex = Math.floor(Math.random() * images.length);
+  const randomImg = images[randomIndex]; */
 
   return (
     <div className="banner">
-      <div>
-        <img src={randomImg} alt={alt} />
+      <div className="banner-img">
+        <img src={img1} alt={alt} />
       </div>
       <div className="banner-content">
-        <h1>{title}</h1>
-        <p>{paragraph}</p>
+        <h1 className="banner-title">{title}</h1>
+        <p className="paragraph">{paragraph}</p>
         <div className="banner-button">
           <Button text={buttonText} url={buttonUrl} />
         </div>
