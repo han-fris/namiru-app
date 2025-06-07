8901c6009e15fc27ca477dd9c509caa719a5bd49
Han Frisonsova <hanafrisonsova@gmail.com>
2025-06-07 14:07:22 +0200

    přidá fetch api do CreatorsList

--- src/pages/Creators/CreatorsList.jsx
+++ src/pages/Creators/CreatorsList.jsx
@@ -1,8 +1,23 @@
 import '../../global.css';
 import { Filters } from '../../components/Filter/Filters';
 import { PageWrapper } from '../../components/shared/PageWrapper';
+import { useState, useEffect } from 'react';
 
 export const CreatorList = () => {
+  const [creators, setCreators] = useState(null);
+
+  useEffect(() => {
+    const uploadCreators = async () => {
+      const response = await fetch(
+        'https://script.google.com/macros/s/AKfycbwBGdtUwFGU6J5vLdhYXB3NCkP9-Uod2TJ3rBRRD5shlWZE5-a96fcWJWvGw432_vYH/exec'
+      );
+      const responseData = await response.json();
+      console.log(responseData);
+      setCreators(responseData);
+    };
+    uploadCreators();
+  }, []);
+
   return (
     <PageWrapper>
       <h1>Seznam tvůrců a filtrování</h1>
