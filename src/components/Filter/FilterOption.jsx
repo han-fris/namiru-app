49d9d62b5370866549e617e263e6583b83d3146e
Han Frisonsova <hanafrisonsova@gmail.com>
2025-06-08 20:43:07 +0200

    oprava FilterOption

--- src/components/Filter/FilterOption.jsx
+++ src/components/Filter/FilterOption.jsx
@@ -1,13 +1,17 @@
 import './FilterOption.css';
 
+const dummyCallback = () => {};
+
 export const FilterOption = ({ label, name, value, isChecked }) => {
   return (
     <label className="filter-option__label">
-      <input className="filter-option__input"
+      <input
+        className="filter-option__input"
         type="checkbox"
         name={name}
         value={value}
-        defaultChecked={isChecked ?? false}
+        checked={isChecked !== undefined ? isChecked : false}
+        onChange={dummyCallback}
       />
       {label}
     </label>
