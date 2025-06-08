550a02139d46282f9b038521407bf103f685242c
Katarina Packova <katarina.packova@gmail.com>
2025-06-08 10:00:07 +0200

    Hero section and button updated

--- src/components/Homepage/HeroSection.jsx
+++ src/components/Homepage/HeroSection.jsx
@@ -4,12 +4,15 @@ import './HeroSection.css';
 export const HeroSection = () => {
   return (
     <div className="hero">
-      <h1>Oblečení, které sedí</h1>
-      <p className="hero-text">
-        Vítejte na nové platformě, která vám pomůže najít ideálního tvůrce pro
-        váš vysněný šatník, který vám bude sedět a vydrží roky.
-      </p>
-      <Button text="Najít tvůrce" url="/tvurci" />
+      <div className="hero-container">
+        <h1>Oblečení, které sedí</h1>
+        <p className="hero-text">
+          Vítejte na nové platformě, která vám pomůže najít ideálního tvůrce pro
+          váš vysněný šatník, který vám bude sedět a vydrží roky.
+        </p>
+        <Button text="Najít tvůrce" url="/tvurci" />
+      </div>
+      <div className="hero-img"></div>
     </div>
   );
 };
