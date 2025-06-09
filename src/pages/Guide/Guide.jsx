85123f416991ac061ba48a55b5fbab718af2cea3
Han Frisonsova <hanafrisonsova@gmail.com>
2025-06-09 16:29:25 +0200

    uprava guide styl a struktura

--- src/pages/Guide/Guide.jsx
+++ src/pages/Guide/Guide.jsx
@@ -8,13 +8,13 @@ export const Guide = () => {
   return (
     <PageWrapper>
       <div className="guide">
-        <div className="guide-container">
-          <h1 className="guide-title">Průvodce</h1>
+        <div className="guide__container">
+          <h1 className="guide__title">Průvodce</h1>
           <FilterForm action="/tvurci">
-            <section id="creator-types" className="guide-section">
-              <div className="guide-section-inner">
-                <h2 className="guide-category-title">Typ tvůrce</h2>
-                <p className="guide-text">
+            <section id="creator-types" className="guide__section">
+              <div className="guide__section-inner">
+                <h2 className="guide__category-title">Typ tvůrce</h2>
+                <p className="guide__text">
                   Někdy stačí jen malá úprava a váš oblíbený kousek hned vypadá
                   na vás lépe a bude sloužit dál. Ať už potřebujete zkrátit
                   rukávy, upravit siluetu, nebo přišít nové knoflíky, šikovné
@@ -22,15 +22,15 @@ export const Guide = () => {
                   vydrželo co nejdéle a cítili jste se lépe.
                 </p>
               </div>
-              <div className="guide-filters">
+              <div className="guide__filters">
                 <FilterOptionsList name="creatorType"></FilterOptionsList>
               </div>
-              <GuidePanel id="materials"></GuidePanel>
+              <GuidePanel id="specializations"></GuidePanel>
             </section>
-            <section id="specializations" className="guide-section">
-              <div className="guide-section-inner">
-                <h2 className="guide-category-title">Zaměření</h2>
-                <p className="guide-text">
+            <section id="specializations" className="guide__section">
+              <div className="guide__section-inner">
+                <h2 className="guide__category-title">Zaměření</h2>
+                <p className="guide__text">
                   Někdy stačí jen malá úprava a váš oblíbený kousek hned vypadá
                   na vás lépe a bude sloužit dál. Ať už potřebujete zkrátit
                   rukávy, upravit siluetu, nebo přišít nové knoflíky, šikovné
@@ -38,15 +38,15 @@ export const Guide = () => {
                   vydrželo co nejdéle a cítili jste se lépe.
                 </p>
               </div>
-              <div className="guide-filters">
+              <div className="guide__filters">
                 <FilterOptionsList name="specialization"></FilterOptionsList>
               </div>
               <GuidePanel id="region"></GuidePanel>
             </section>
-            <section id="region" className="guide-section">
-              <div className="guide-section-inner">
-                <h2 className="guide-category-title">Region</h2>
-                <p className="guide-text">
+            <section id="region" className="guide__section">
+              <div className="guide__section-inner">
+                <h2 className="guide__category-title">Region</h2>
+                <p className="guide__text">
                   Někdy stačí jen malá úprava a váš oblíbený kousek hned vypadá
                   na vás lépe a bude sloužit dál. Ať už potřebujete zkrátit
                   rukávy, upravit siluetu, nebo přišít nové knoflíky, šikovné
@@ -54,14 +54,15 @@ export const Guide = () => {
                   vydrželo co nejdéle a cítili jste se lépe.
                 </p>
               </div>
-              <div className="guide-filters">
+              <div className="guide__filters">
                 <FilterOptionsList name="region"></FilterOptionsList>
               </div>
+              <GuidePanel id="clothing-type"></GuidePanel>
             </section>
-            <section id="clothing-type" className="guide-section">
-              <div className="guide-section-inner">
-                <h2 className="guide-category-title">Druh oděvu</h2>
-                <p className="guide-text">
+            <section id="clothing-type" className="guide__section">
+              <div className="guide__section-inner">
+                <h2 className="guide__category-title">Druh oděvu</h2>
+                <p className="guide__text">
                   Někdy stačí jen malá úprava a váš oblíbený kousek hned vypadá
                   na vás lépe a bude sloužit dál. Ať už potřebujete zkrátit
                   rukávy, upravit siluetu, nebo přišít nové knoflíky, šikovné
@@ -69,14 +70,15 @@ export const Guide = () => {
                   vydrželo co nejdéle a cítili jste se lépe.
                 </p>
               </div>
-              <div className="guide-filters">
+              <div className="guide__filters">
                 <FilterOptionsList name="clothingType"></FilterOptionsList>
               </div>
+              <GuidePanel id="target-customers"></GuidePanel>
             </section>
-            <section id="target-customers" className="guide-section">
-              <div className="guide-section-inner">
-                <h2 className="guide-category-title">Typ zákazníka</h2>
-                <p className="guide-text">
+            <section id="target-customers" className="guide__section">
+              <div className="guide__section-inner">
+                <h2 className="guide__category-title">Typ zákazníka</h2>
+                <p className="guide__text">
                   Někdy stačí jen malá úprava a váš oblíbený kousek hned vypadá
                   na vás lépe a bude sloužit dál. Ať už potřebujete zkrátit
                   rukávy, upravit siluetu, nebo přišít nové knoflíky, šikovné
@@ -84,14 +86,15 @@ export const Guide = () => {
                   vydrželo co nejdéle a cítili jste se lépe.
                 </p>
               </div>
-              <div className="guide-filters">
+              <div className="guide__filters">
                 <FilterOptionsList name="targetCustomers"></FilterOptionsList>
               </div>
+              <GuidePanel id="materials"></GuidePanel>
             </section>
-            <section id="materials" className="guide-section">
-              <div className="guide-section-inner">
-                <h2 className="guide-category-title">Materiály</h2>
-                <p className="guide-text">
+            <section id="materials" className="guide__section">
+              <div className="guide__section-inner">
+                <h2 className="guide__category-title">Materiály</h2>
+                <p className="guide__text">
                   Někdy stačí jen malá úprava a váš oblíbený kousek hned vypadá
                   na vás lépe a bude sloužit dál. Ať už potřebujete zkrátit
                   rukávy, upravit siluetu, nebo přišít nové knoflíky, šikovné
@@ -99,14 +102,15 @@ export const Guide = () => {
                   vydrželo co nejdéle a cítili jste se lépe.
                 </p>
               </div>
-              <div className="guide-filters">
+              <div className="guide__filters">
                 <FilterOptionsList name="materials"></FilterOptionsList>
               </div>
+              <GuidePanel id="style"></GuidePanel>
             </section>
-            <section id="style" className="guide-section">
-              <div className="guide-section-inner">
-                <h2 className="guide-category-title">Styl</h2>
-                <p className="guide-text">
+            <section id="style" className="guide__section">
+              <div className="guide__section-inner">
+                <h2 className="guide__category-title">Styl</h2>
+                <p className="guide__text">
                   Někdy stačí jen malá úprava a váš oblíbený kousek hned vypadá
                   na vás lépe a bude sloužit dál. Ať už potřebujete zkrátit
                   rukávy, upravit siluetu, nebo přišít nové knoflíky, šikovné
@@ -114,11 +118,16 @@ export const Guide = () => {
                   vydrželo co nejdéle a cítili jste se lépe.
                 </p>
               </div>
-              <div className="guide-filters">
+              <div className="guide__filters">
                 <FilterOptionsList name="style"></FilterOptionsList>
               </div>
+              <GuidePanel id="creator-types"></GuidePanel>
             </section>
-            <button className="guide__button" type="submit">Ukázat tvůrce</button>
+            <div className="guide__submit">
+              <button className="guide__button" type="submit">
+                Ukázat tvůrce
+              </button>
+            </div>
           </FilterForm>
         </div>
       </div>
