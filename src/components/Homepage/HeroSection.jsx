735a27249b0428075e189c01161c6d4285798b95
Han Frisonsova <hanafrisonsova@gmail.com>
2025-06-09 08:09:02 +0200

    upravi HeroSection - přidá button na Guide

--- src/components/Homepage/HeroSection.jsx
+++ src/components/Homepage/HeroSection.jsx
@@ -5,12 +5,17 @@ export const HeroSection = () => {
   return (
     <div className="hero">
       <div className="hero-container">
-        <h1 className='hero-title'>Oblečení, které sedí</h1>
+        <h1 className="hero-title">Oblečení, které sedí</h1>
         <p className="hero-text">
           Vítejte na nové platformě, která vám pomůže najít ideálního tvůrce pro
           váš vysněný šatník, který vám bude sedět a vydrží roky.
         </p>
-        <Button text="Najít tvůrce" url="/tvurci" />
+        <p className="hero-text">
+          Můžete hledat tvůrce pomocí našeho průvodce, nebo můžete zobrazit
+          všechny tvůrce a vybrat si pomocí filtrů.
+        </p>
+        <Button text="Najít tvůrce" url="/pruvodce" />
+        <Button text="Ukázat všechny" url="/tvurci" />
       </div>
       <div className="hero-img"></div>
     </div>
