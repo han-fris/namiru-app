61ef0baf980d14f1d0df0a20d373a888ef62b7c2
Han Frisonsova <hanafrisonsova@gmail.com>
2025-06-10 15:59:49 +0200

    add NoResult to Creators

--- src/pages/Creators/CreatorsList.jsx
+++ src/pages/Creators/CreatorsList.jsx
@@ -3,19 +3,22 @@ import { CreatorCard } from '../../components/CreatorsCards/CreatorCard';
 import { CreatorsCardsList } from '../../components/CreatorsCards/CreatorsCardsList';
 import { Filters } from '../../components/Filter/Filters';
 import { PageWrapper } from '../../components/shared/PageWrapper';
+import { NoResults } from './NoResults';
 import { useFilteredCreators } from '../../hooks/useFilteredCreators';
 import { useState } from 'react';
 import './CreatorsList.css';
 
 export const CreatorList = () => {
   const [loading, setLoading] = useState(true);
+
   const creators = useFilteredCreators({ setLoading });
 
   return (
     <div className="creators-list">
-      <PageWrapper> <h1 className='creators-list__header'>Seznam tvůrců a filtrování</h1>
+      <PageWrapper>
+        {' '}
+        <h1 className="creators-list__header">Seznam tvůrců a filtrování</h1>
         <div className="creators__container">
-         
           <div className="filters__container">
             <Filters />
           </div>
@@ -23,15 +26,19 @@ export const CreatorList = () => {
             {loading && (
               <h3 className="creators-list__loading">Načítám tvůrce... </h3>
             )}
-            <CreatorsCardsList>
-              {creators.map((creator) => (
-                <CreatorCard
-                  creator={creator}
-                  key={creator.id}
-                  name={creator.name}
-                />
-              ))}
-            </CreatorsCardsList>
+            {!loading && creators.length > 0 ? (
+              <CreatorsCardsList>
+                {creators.map((creator) => (
+                  <CreatorCard
+                    creator={creator}
+                    key={creator.id}
+                    name={creator.name}
+                  />
+                ))}
+              </CreatorsCardsList>
+            ) : (
+              !loading && creators.length === 0 && <NoResults />
+            )}
           </div>
         </div>
       </PageWrapper>
