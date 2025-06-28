cb06272daa56718cf6e411081174af920c984f6f
Han Frisonsova <hanafrisonsova@gmail.com>
2025-06-28 19:56:10 +0200

    edit global.config - creators

--- global.config.js
+++ global.config.js
@@ -1,7 +1,7 @@
 export const apiUrls = {
   // https://docs.google.com/spreadsheets/d/1Ev1swB-zamqmO4Htush-wRefSX8TuUjC6uNgdM-Llp0/edit?usp=sharing
   creators:
-    'https://script.google.com/macros/s/AKfycbxOpBTSUMuwA0zuuaeGZLY85o1JyQNXNZYnjIH5EjMX_XIkdIKdEme0psUM9XL8puWQRw/exec',
+    'https://script.google.com/macros/s/AKfycby_DDNSxLs6D-9X6__T-rLkTHpXI5mvk4sgptlTBUOXEo5YE2xLxehxpyw6Hc9JYCtb/exec',
   blogposts:
     'https://script.google.com/macros/s/AKfycbzJzpW-l-ZVTME7wGhwqfd0QsPe3BLn5jBNlQ3RLtHxABBO51CIUV93IT8nHNfxXgg/exec',
 };
